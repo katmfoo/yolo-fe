e40e93b0c7a8b88b69f0f6b6b2a30de5678c5904
Dylanshapiro <shapiro1997@gmail.com>
2018-10-07 10:38:46 -0400

    Added configure gpu script

--- configure_gpu.py
+++ configure_gpu.py
@@ -1,2 +1,6 @@
 def configure_gpu():
     print('Now configuring YOLO to utilize GPU...')
+    with open("Makefile", "rt") as fin:
+        with open("Makefile", "wt") as fout:
+            for line in fin:
+                fout.write(line.replace('GPU=0', 'GPU=1'))
