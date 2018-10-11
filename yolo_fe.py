7362145519f52b5e41c33688c7c9fe5de49362a7
pricheal <patrickricheal@gmail.com>
2018-10-11 11:22:03 -0400

    Add options to setup command

--- yolo_fe.py
+++ yolo_fe.py
@@ -1,15 +1,23 @@
 import click
-from downloader import download
+from yolo_setup import download, configure, install
 
 @click.group()
 def cli():
     pass
 
 @cli.command()
-def setup():
+@click.option('--gpu/--cpu', default=True)
+@click.option('--cuda-path', default=False)
+def setup(gpu, cuda_path):
     '''Automatically downloads, configures, and builds YOLO to the yolo/ directory.'''
 
-    # Run script to automatically download and setup YOLO
     download()
+
+    if cuda_path:
+        configure(enable_gpu=gpu, cuda_path=cuda_path)
+    else:
+        configure(enable_gpu=gpu)
+
+    install()
 
     print('YOLO has been setup successfully')
