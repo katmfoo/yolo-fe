e4903fc517db867f7892d1de9ac4eea34dd98a78
Dylanshapiro <shapiro1997@gmail.com>
2018-11-03 13:08:08 -0400

    added transparent method

--- edgeDetection.py
+++ edgeDetection.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import cv2 as cv
+from PIL import Image
 
 def edgeDetection(dataset):
     print("Beginning edge detection for new dataset.")
@@ -23,6 +24,7 @@ def edgeDetection(dataset):
             if(os.path.isfile(img_2)):
                 runCV(img_2)
 
+
     print("Completed edge detection on " + dataset + " dataset.")
     print("Edge detected images located in " + newDataset + " directory.")
 
@@ -31,6 +33,23 @@ def runCV(image):
     edges = cv.Canny(img,100,200)
     cv.imwrite(image,edges)
 
+def convertToPNG(tempImage):
+    img = tempImageb #image
+    img = img.convert("RGBA")
+    datas = img.getdata()
+    newData = []
+    for item in datas:
+        if item[0] == 255 and item[1] == 255 and item[2] == 255:
+            newData.append((255, 255, 255, 0))
+        else:
+            newData.append(item)
+    img.putdata(newData)
+    #converted Image name
+    print('Done')
+    return img
+
+
 if __name__ == "__main__":
 
     edgeDetection('people_dataset')
+
